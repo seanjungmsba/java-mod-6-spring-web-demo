2ebb7f023ef9512550d6f4d7b8b5798da91f4c79
seanjungmsba <seanjeong1996@gmail.com>
2022-08-05 14:18:14 -0400

    Followed along Spring Web MVC module

---
+++ src/main/java/com/example/springwebdemo/repository/MemberRepository.java
@@ -0,0 +1,11 @@
+package com.example.springwebdemo.repository;
+
+import com.example.springwebdemo.model.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Integer> {
+    // Notice that we are extending the JpaRepository interface (Links to an external site.) instead of CrudRepository (Links to an external site.)`. The
+    //JpaRepository interface a few JPA specific extensions that makes it more convenient. For example, its findAll method returns a List instead of an Iterable which makes it easier to serialize the data.
+}
